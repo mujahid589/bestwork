5142faab9d7ce7228058e1265a4cbbcef5af8674
Mohsin Baig <75554445+Mohsinbaig103@users.noreply.github.com>
2021-01-16 21:47:32 +0500

    Add files via upload

--- logout.php
+++ logout.php
@@ -1,4 +1,10 @@
-<?php session_start();
-session_destroy();
-header('location:index.php')
+<?php
+session_start();
+$_SESSION['alogin']=="";
+session_unset();
+//session_destroy();
+$_SESSION['errmsg']="You have successfully logout";
 ?>
+<script language="javascript">
+document.location="index.php";
+</script>
